fd35d580999dec4eb9e914e2b298ce07de77ee50
Zhicheng Liu <zcliu1922@gmail.com>
2025-02-07 18:17:14 -0500

    Environment setup first try

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Roses are red, violets are blue. An unexpected '(' on line 32.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
